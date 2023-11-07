e988dbd772762ba69497e34e8479a5ad5fce9da9
linha06 <syauqiaidan@gmail.com>
2023-11-07 23:23:37 +0700

    pindah main activity ke dalam folder ui.main

--- Diabetless App/app/src/main/java/com/CH2PS073/diabetless/ui/main/MainActivity.kt
+++ Diabetless App/app/src/main/java/com/CH2PS073/diabetless/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.CH2PS073.diabetless
+package com.CH2PS073.diabetless.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.CH2PS073.diabetless.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
